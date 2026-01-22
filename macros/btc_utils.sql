eaee963cd10b8085dfe8b52710a5617c4af04427
twishadoshi14 <twisha.doshi@ics-global.in>
2026-01-22 18:23:20 +0530

    reverting chamges

--- macros/btc_utils.sql
+++
@@ -1,6 +0,0 @@
-{% macro convert_to_usd(column_name) %}
-{{column_name}} *( 
-    SELECT PRICE 
-    FROM {{ ref('btc_usd_max') }}
-    where to_date(REPLACE(snapped_at,'UTC','')) = current_date())
-{% endmacro %}
--- models/stg/stg_btc_transactions.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='ephemeral') }}
-
-SELECT *
-FROM {{ ref('stg_btc_outputs') }}
-where is_coinbase = false
